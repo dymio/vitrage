ab38d93fc05728ba9fc27a0533eae3daf47e5301
Ivan Dymkov <mstrdymio@gmail.com>
2015-02-04 13:55:59 +0700

    Move routes method to special module

--- lib/vitrage.rb
+++ lib/vitrage.rb
@@ -1,23 +1,8 @@
 require "vitrage/engine"
+require "vitrage/router"
 require "vitrage/acts_as_vitrage_owner"
 require "vitrage/helpers/action_view_extention"
 
 module Vitrage
-  class << self
-    def routes(rails_router, options = {})
-      if options[:controller]
-        cs = options[:controller].to_s
-        rails_router.post   '/vitrage/pieces'          => "#{cs}#create", as: :vitrage_pieces
-        rails_router.get    '/vitrage/pieces/new'      => "#{cs}#new",    as: :new_vitrage_piece
-        rails_router.get    '/vitrage/pieces/:id/edit' => "#{cs}#edit",   as: :edit_vitrage_piece
-        rails_router.get    '/vitrage/pieces/:id'       => "#{cs}#show",  as: :vitrage_piece
-        rails_router.match  '/vitrage/pieces/:id'       => "#{cs}#update", via: [:patch, :put]
-        rails_router.delete '/vitrage/pieces/:id'       => "#{cs}#destroy"
-      else
-        rails_router.namespace :vitrage do
-          rails_router.resources :pieces, except: [:index]
-        end
-      end
-    end
-  end
+  extend Vitrage::Router
 end
---
+++ lib/vitrage/router.rb
@@ -0,0 +1,21 @@
+module Vitrage
+  module Router
+
+    def routes(rails_router, options = {})
+      if options[:controller]
+        cs = options[:controller].to_s
+        rails_router.post   '/vitrage/pieces'           => "#{cs}#create", as: :vitrage_pieces
+        rails_router.get    '/vitrage/pieces/new'       => "#{cs}#new",    as: :new_vitrage_piece
+        rails_router.get    '/vitrage/pieces/:id/edit'  => "#{cs}#edit",   as: :edit_vitrage_piece
+        rails_router.get    '/vitrage/pieces/:id'       => "#{cs}#show",   as: :vitrage_piece
+        rails_router.match  '/vitrage/pieces/:id'       => "#{cs}#update", via: [:patch, :put]
+        rails_router.delete '/vitrage/pieces/:id'       => "#{cs}#destroy"
+      else
+        rails_router.namespace :vitrage do
+          rails_router.resources :pieces, except: [:index]
+        end
+      end
+    end
+
+  end
+end
